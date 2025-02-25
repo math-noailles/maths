e0a6ab1930eaccb0eb79b1516d34765efd73b648
attar <math.courtieu@gmail.com>
2025-02-25 19:33:07 +0100

    25/02/2025 19h33

---
+++ Seconde/Fonctions/Affines/aff_dro_exo2.js
@@ -0,0 +1,61 @@
+function definir()
+{
+    hyperlien1 = '<a href="Seconde/Fonctions/Affines/affines_droite.html"><span class="parent">Droite</span></a>';
+    spanLien = document.getElementById('lien1');
+    spanLien.innerHTML = hyperlien1;
+
+    nb_questions = 4;
+    nb_prop = 2;
+    nb_pos = 8;
+
+    q1 = "Soit le graphique suivant : ";
+
+    tab_q = []
+
+    for (i=0; i<nb_pos; i++)
+    {
+        tab_q.push(q1);
+    }
+
+    tab_im = [
+                `aff_dro_exo1_A.png`,
+                `aff_dro_exo1_B.png`,
+                `aff_dro_exo1_C.png`,
+                `aff_dro_exo1_D.png`,
+                `aff_dro_exo1_E.png`,
+                `aff_dro_exo1_F.png`,
+                `aff_dro_exo1_G.png`,
+                `aff_dro_exo1_H.png`
+            ];
+
+    e1 = "Le coefficient directeur de $f$ est : ";
+    e2 = "L'ordonnée à l'origine de $f$ est : ";
+
+    tab_e = []
+    
+    for (i=0; i<nb_pos; i++)
+    {
+        tab_e.push([e1, e2]);
+    }
+
+    m1 = "C'est la variation en $y$ divisée par la variation en $x$.";
+    m2 = "Regarder l'intersection avec l'axe des $y$.";
+
+    tab_m = []
+    
+    for (i=0; i<nb_pos; i++)
+    {
+        tab_m.push([m1, m2]);
+    }
+
+    tab_rep = [
+                    ["3", "-1"], 
+                    ["0.25", "1"],
+                    ["-1", "2"],
+                    ["-1", "3"],
+                    ["2", "0"],
+                    ["0", "2"],
+                    ["1", "1"],
+                    ["-0.5", "1"]
+              ];
+}
---
+++ drag_drop.js
@@ -0,0 +1,160 @@
+function allowDrop(ev) 
+{
+  ev.preventDefault();
+}
+
+function drag(ev) 
+{
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) 
+{
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+function creer_tab()
+{
+        ta = [];
+        for (i = 0; i < nb_pos; i++)
+        {
+          ta.push(i);
+        }
+        for (i = ta.length - 1; i > 0; i--) 
+        {
+            j = Math.floor(Math.random() * (i + 1));
+            [ta[i], ta[j]] = [ta[j], ta[i]];
+        }
+        return ta;
+}
+
+function afficher_enonce(num, localStorage, chemin)
+{
+    localS = "'" + localStorage + "'";
+    ta = creer_tab();
+    codeHTML = "";
+    for (nq=1; nq<=num; nq++)
+    {
+        codeHTML = codeHTML + `<div class="question"> <b> ` + nq + `. </b>` + tab_q[ta[nq]];
+        if (tab_im.length > 0)
+        {
+                        codeHTML = codeHTML + `   <br> <img src="` + chemin + `/images/` + tab_im[ta[nq]] + `"/>`;
+        }
+        codeHTML = codeHTML + `<br><br>`;
+        
+        codeHTML = codeHTML + ` <div id="o1" class="drop1" ondrop="drop(event)" ondragover="allowDrop(event)">
+                                    <div id="dr1" class="drag1" draggable="true" ondragstart="drag(event)">
+                                        Google
+                                    </div>
+                                </div>
+                                <br><br><br><br>
+                            `;
+
+        codeHTML = codeHTML + ` <div>
+                                    <label>
+                                        &emsp;FAI
+                                    </label><br>
+                                    <div id="d1" class="drop2" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+                                </div>
+                                <br><br><br><br>
+                                `;
+        /*
+        for (ne=0; ne<nb_prop; ne++)
+        {
+            codeHTML = codeHTML + tab_e[ta[nq]][ne];
+            num_form = (nq - 1) * nb_prop + ne;
+            codeHTML = codeHTML + `
+                    <form id="f` + num_form + `">
+                    <input type='text' class='zone_saisie1' id='inp1' autocomplete='off'
+                        onkeypress="javascript:if (event.keyCode == 13) {verif('f` + num_form + `', ` + nq + `, ` + ne + `);return false;}"/>
+                    `;
+            codeHTML = codeHTML +
+                        `
+                        <div>
+                            <span id='sp1' class='ic_rep'> &emsp; </span>
+                            <input class="bouton1" type="button" value="Valider" onClick="verif('f` + num_form + `', ` + nq + `, ` + ne + `)" />
+                            <br>
+                            <span class='juste2' id="sp2"> &ensp; </span>
+                        </div>
+                    </form><br>`;
+        }
+        codeHTML = codeHTML +
+                    ` </div>
+                    
+            `;
+        codeHTML = codeHTML +
+                    `
+                    <span id='m1'></span> <br>
+                    `;
+        */
+    }
+    num_q = num * nb_prop;
+    codeHTML = codeHTML +
+                    `
+                    <p>
+                        <input id='bv' class="bouton2" type="button" value="Valider l'exercice" onClick="verif_exo(`
+                        + localS +
+                        `,`
+                        + num_q + `)" />
+                        <span class="span_exo"></span>
+                    </p>
+                    `;
+    
+    spanForm = document.getElementById('form');
+    spanForm.innerHTML = codeHTML;
+    // Réactualiser MathJax pour reprocesser les expressions LaTeX
+    MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+}
+
+
+function verif(nom_form,  k, l)
+{
+    form = document.forms[nom_form];
+    input_saisie = form.querySelector("#inp1");
+    repon = input_saisie.value;
+    repon = repon.toLowerCase();
+    repon = repon.replace(/ /g,"");
+    repon = repon.replace(/,/g,".");
+    repon = repon.toString();
+    repon = eval(repon);
+    span_ic = form.querySelector("#sp1");
+    span_mes = form.querySelector("#sp2");
+    if (tab_rep[ta[k]][l] == repon)
+    {
+        span_ic.innerHTML = "<b class='juste'>&#10004;</b>";
+        span_mes.innerHTML = `<b class='juste'>Bonne réponse !</b>`;
+    }
+    else
+    {
+        span_ic.innerHTML = "<b class='faux'>&#10060;</b>";
+        mes_err = `<b class="faux2">` + tab_m[ta[k]][l] + `</b>`;
+        span_mes.innerHTML = mes_err;
+         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+    }
+}
+
+
+function verif_exo(chaine, n)
+{
+    tab_juste = document.querySelectorAll(".juste");
+    m = document.querySelector(".span_exo");
+    l = tab_juste.length;    
+    bout = document.getElementById('bv');
+    if (l == 2 * n)
+    {
+        m.innerHTML = "<b class='juste2'>Exercice validé.</b>";
+        if(typeof localStorage!='undefined') 
+        {
+            localStorage.setItem(chaine, 1);
+            bout.style.backgroundColor = "green";
+
+        }
+    }
+    else
+    {
+        m.innerHTML = "<b class='faux2'>Exercice non validé.</b>";
+        bout.style.backgroundColor = "red";
+    }
+}
